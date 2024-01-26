56e563ac1d5c9bacb2cc0956d6301d38480b0b4c
Tianyu Zhang <tianyu7@illinois.edu>
2024-01-26 16:29:22 +0800

    add authbase frontend

--- ui/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './static/scripts/system/scan.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
